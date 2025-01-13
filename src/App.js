2b1af22c0d2bc7cbdae285a45549a4bfeb2020f6
Vivek <vivek.mitra@outlook.com>
2025-01-13 20:23:55 +1100

    initial project setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { Alchemy, Network } from 'alchemy-sdk';
-import { useEffect, useState } from 'react';
+import { Alchemy, Network } from "alchemy-sdk";
+import { useEffect, useState } from "react";
 
-import './App.css';
+import "./App.css";
 
 // Refer to the README doc for more information about using API
 // keys in client-side code. You should never do this in production
@@ -10,7 +10,6 @@ const settings = {
   apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
   network: Network.ETH_MAINNET,
 };
-
 
 // In this week's lessons we used ethers.js. Here we are using the
 // Alchemy SDK is an umbrella library with several different packages.
@@ -21,6 +20,7 @@ const alchemy = new Alchemy(settings);
 
 function App() {
   const [blockNumber, setBlockNumber] = useState();
+  const [blockInfo, setBlockInfo] = useState();
 
   useEffect(() => {
     async function getBlockNumber() {
@@ -29,8 +29,29 @@ function App() {
 
     getBlockNumber();
   });
+  useEffect(() => {
+    async function getBlockInfo() {
+      setBlockInfo(await alchemy.core.getBlock("latest"));
+    }
+  });
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+  return (
+    <div className="App">
+      Block Number: {blockNumber}
+      <div className="BlockInfo">
+        <h2>Block Information</h2>
+        <p>Hash: {blockInfo.hash}</p>
+        <p>Timestamp: {blockInfo.timestamp}</p>
+        <p>Difficulty: {blockInfo.difficulty}</p>
+        <p>Nonce: {blockInfo.nonce}</p>
+        <p>Miner: {blockInfo.miner}</p>
+      </div>
+      <div className="TransactionInformationForm">
+        <h2>Block Transaction Information</h2>
+        <p>Enter the block number or hash of the block you want to inspect: </p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
